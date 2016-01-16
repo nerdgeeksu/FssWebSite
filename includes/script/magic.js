71ca0afc07cdf91baf0cc47f96b0e081dbca29c7
diopisemou <diopisemou@gmail.com>
2016-01-16 12:16:51 +0000

    Sign In Test

--- includes/script/magic.js
+++ includes/script/magic.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
+
+						//  ==================  Connexion Processing   ===================
 // process the form
 $('#connexionform').submit(function(event) {
 	
 	$(".ajax_spinner").remove();
     $(".ajax_wait").remove();
 
-   var label = "<span class='ajax_spinner' align=center><img src='includes/script/files/ispinner.gif'/> Loading </span>";
+   var label = "<span class='ajax_spinner' align=center><img src='includes/script/files/ispinner.gif'/> Chargement </span>";
    $(".ajax_wait").after(label);
    
   // remove the past errors
@@ -61,6 +63,7 @@ $('#connexionform').submit(function(event) {
         // if validation is good add success message
         $('#messages').addClass('alert alert-success').append('<p>' + data.message + '</p>');
 		$('#connexion-modal').modal('hide');
+		$(document).reload();
       }
     }
   });
@@ -70,73 +73,77 @@ $('#connexionform').submit(function(event) {
 });
 
 
-// process the form
-$('#inscriptionform').submit(function(event) {
-	
-	$(".ajax_spinner").remove();
-    $(".ajax_wait").remove();
 
-   var label = "<span class='ajax_spinner' align=center><img src='includes/script/files/ispinner.gif'/> Loading </span>";
-   $(".ajax_wait").after(label);
-   
-  // remove the past errors
-  $('#name-group').removeClass('has-error');
-  $('#name-group .help-block').empty();
-  $('#superhero-group').removeClass('has-error');
-  $('#superhero-group .help-block').empty();
+            //  ==================  Inscription Processing   ===================
 
-  // remove success messages
-  $('#messages').removeClass('alert alert-success').empty();
+			// process the form
+			$('#inscriptionform').submit(function(event) {
+				
+				$(".ajax_spinner").remove();
+				$(".ajax_wait").remove();
 
-  // get the form data
-  var formData = {
-      'name'              : $('input[name=name]').val(),
-      'superheroAlias'    : $('input[name=superheroAlias]').val()
-  };
+			   var label = "<span class='ajax_spinner' align=center><img src='includes/script/files/ispinner.gif'/> Chargement </span>";
+			   $(".ajax_wait").after(label);
+			   
+			  // remove the past errors
+			  $('#name-group').removeClass('has-error');
+			  $('#name-group .help-block').empty();
+			  $('#superhero-group').removeClass('has-error');
+			  $('#superhero-group .help-block').empty();
 
-  // process the form
-  $.ajax({
-    type        : 'POST',
-    url         : 'includes/script/inscriptionverif.php',
-    data        : formData,
-    dataType    : 'json',
-    success     : function(data) {
+			  // remove success messages
+			  $('#messages').removeClass('alert alert-success').empty();
 
-	 
-	 
-      
+			  // get the form data
+			  var formData = {
+				  'name'              : $('input[name=name]').val(),
+				  'superheroAlias'    : $('input[name=superheroAlias]').val()
+			  };
 
-      // if validation fails
-      // add the error class to show a red input
-      // add the error message to the help block under the input
-      if ( ! data.success) {
-		  
-		  $(".ajax_spinner").remove();
-		  $(".ajax_wait").remove();
-		  
-        if (data.errors.name) {
-          $('#name-group').addClass('has-error');
-          $('#name-group .help-block').html(data.errors.name);
-        }
+			  // process the form
+			  $.ajax({
+				type        : 'POST',
+				url         : 'includes/script/inscriptionverif.php',
+				data        : formData,
+				dataType    : 'json',
+				success     : function(data) {
 
-        if (data.errors.superheroAlias) {
-          $('#superhero-group').addClass('has-error');
-          $('#superhero-group .help-block').html(data.errors.superheroAlias);
-        }
+				 
+				 
+				  
 
-      } else {
-		  
-		  var label = "<span class='ajax_spinner' align=center><img src='includes/script/files/ispinner.gif'/> Chargement</span>";
-          $(".ajax_wait").after(label);
-        // if validation is good add success message
-        $('#messages').addClass('alert alert-success').append('<p>' + data.message + '</p>');
-		$('#inscription-modal').modal('hide');
-      }
-    }
-  });
+				  // if validation fails
+				  // add the error class to show a red input
+				  // add the error message to the help block under the input
+				  if ( ! data.success) {
+					  
+					  $(".ajax_spinner").remove();
+					  $(".ajax_wait").remove();
+					  
+					if (data.errors.name) {
+					  $('#name-group').addClass('has-error');
+					  $('#name-group .help-block').html(data.errors.name);
+					}
 
-  // stop the form from submitting and refreshing
-  event.preventDefault();
-});
+					if (data.errors.superheroAlias) {
+					  $('#superhero-group').addClass('has-error');
+					  $('#superhero-group .help-block').html(data.errors.superheroAlias);
+					}
+
+				  } else {
+					  
+					  var label = "<span class='ajax_spinner' align=center><img src='includes/script/files/ispinner.gif'/> Chargement</span>";
+					  $(".ajax_wait").after(label);
+					// if validation is good add success message
+					$('#messages').addClass('alert alert-success').append('<p>' + data.message + '</p>');
+					$('#inscription-modal').modal('hide');
+					$(document).reload();
+				  }
+				}
+			  });
+
+			  // stop the form from submitting and refreshing
+			  event.preventDefault();
+			});
 
 });
